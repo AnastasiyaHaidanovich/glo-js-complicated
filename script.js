127de1f5f65a63377423253929a54e36ab0a1f2c
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2021-12-09 14:27:48 +0300

    add: spanRange value

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let inputText = document.querySelector("[type=text]");
 let eBtn = document.getElementById("e_btn");
 
 let inputRange = document.querySelector("[type=range]");
+let spanRange = document.getElementById("range-span");
 let circle = document.getElementById("circle");
 
 btn.addEventListener('click', function() {
@@ -14,6 +15,7 @@ btn.addEventListener('click', function() {
 eBtn.style.display = "none";
 
 inputRange.addEventListener('input', function(){
+    spanRange.innerText = inputRange.value;
     circle.style.width = inputRange.value + "%";
     circle.style.height = inputRange.value + "%";
 });
