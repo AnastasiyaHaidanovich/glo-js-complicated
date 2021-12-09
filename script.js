fd78e5eb27b28e74352f1837910eeba97a38f68b
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2021-12-09 14:20:22 +0300

    add: complete task

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let btn = document.getElementById("btn");
+let inputText = document.querySelector("[type=text]");
 
+let eBtn = document.getElementById("e_btn");
 
+let inputRange = document.querySelector("[type=range]");
+let circle = document.getElementById("circle");
 
+btn.addEventListener('click', function() {
+    btn.style.backgroundColor = inputText.value;
+    inputText.value = "";
+});
 
+eBtn.style.display = "none";
 
+inputRange.addEventListener('input', function(){
+    circle.style.width = inputRange.value + "%";
+    circle.style.height = inputRange.value + "%";
+});
