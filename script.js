27b2c9488a31a2e67c3384231d5032c739b0aadf
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2021-12-22 10:55:56 +0300

    add: debounced function

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const addText = document.querySelector(".add-text");
 const showText = document.querySelector(".show-text");
 
-function debounce() {
-    addText.addEventListener('change', () => {
-        showText.textContent = "";
-        const showTextFunc = () => {
-            showText.textContent = addText.value;
-            addText.value = "";
+function debounce(fn, ms) {
+    let timeout;
+    return function () {
+        const fnCall = () => { 
+            fn.apply(this, arguments);
         };
-        setTimeout(showTextFunc, 300);
-});
+        clearTimeout(timeout);
+        timeout = setTimeout(fnCall, ms);
+    };
 }
-debounce();
+
+const showTextFunc = () => {
+    showText.textContent = addText.value;
+    addText.value = "";
+};
+
+let showTextDebounced = debounce(showTextFunc, 700);
+
+addText.addEventListener('keyup',showTextDebounced);
